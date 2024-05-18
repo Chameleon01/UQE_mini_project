24cf5d2223f09283611e419839c005c9a384cc69
guiduc <guillaume.duc.95@gmail.com>
2024-05-18 11:07:43 +0200

    Change output of the model into microfarad

--- model_evaluation_normalized.m
+++ model_evaluation_normalized.m
@@ -1,5 +1,7 @@
 function model_response = model_evaluation_normalized(U_e, U_v, U_r)
     % This function return the value of the capacitance given the
     % normalized inputs
-    model_response = (30 + 0.12 .* U_v) ./ (100 .* pi .* (1000 + 1.5 .* U_r) .* sqrt((100 + 0.45 .* U_e) .^ 2 - (30 + 0.12 .* U_v) .^ 2));
+
+    unitConvertion = 1e6; % μF / F
+    model_response = unitConvertion .* (30 + 0.12 .* U_v) ./ (100 .* pi .* (1000 + 1.5 .* U_r) .* sqrt((100 + 0.45 .* U_e) .^ 2 - (30 + 0.12 .* U_v) .^ 2));
 end
