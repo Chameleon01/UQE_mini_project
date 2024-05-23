2d9d4ac76594fefb4f91f970aa5775e3fa546a07
guiduc <guillaume.duc.95@gmail.com>
2024-05-23 11:08:12 +0200

    Implement the statistical hermite polynomial

--- eval_hermite.m
+++ eval_hermite.m
@@ -7,24 +7,24 @@ if deg < 0
     error('The degree should be at least 0!')
 end
 
-% Compute the Hermite polynomials
+% Compute the Hermite polynomials (Lecture 7 S. 26)
 if deg == 0
     H = ones(size(X)); % H_0 = 1
 elseif deg == 1
-    H = 2*X; % H_1 = 2X
+    H = X; % H_1 = X
 else
     % Initialize recursion
     H_nminus1 = ones(size(X)); % H_0
-    H_n = 2*X; % H_1
-    
+    H_n = X; % H_1
+
     for n = 1:(deg-1)
         % Apply the recursion rule for deg >= 2
-        H_nplus1 = 2*X.*H_n - 2*n*H_nminus1; % Element-wise multiplication
+        H_nplus1 = X.*H_n - n*H_nminus1; % Element-wise multiplication
         % Update for the next iteration
         H_nminus1 = H_n; % Update H_nminus1 first
         H_n = H_nplus1; % Now update H_n
     end
-    
+
     % H contains the evaluations of the Hermite polynomial of degree deg
     H = H_nplus1;
 end
